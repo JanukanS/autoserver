2b24b364031772980ee84d2eda51ba5737673c6b
JanukanS <28988453+JanukanS@users.noreply.github.com>
2023-01-07 13:00:51 -0700

    Created classes for function data and template storage

--- autoserver/server.py
+++ autoserver/server.py
@@ -3,22 +3,40 @@ from fastapi import FastAPI
 import uvicorn
 import jinja2
 import pathlib
+import inspect
 
-class AutoServer():
+class TemplateBase():
+    jinjaPath = pathlib.Path(__file__).resolve().parent / 'resources/'
+    jinjaEnv = jinja2.Environment(loader=jinja2.FileSystemLoader(jinjaPath))
+    homepage = jinjaEnv.get_template("homepage.html.jinja2")
+
+class funcData(TemplateBase):
+    def __init__(self, newfunc):
+        self.fn = newfunc
+        self.name = newfunc.__name__
+        self.typeDict = self.inputTypeDict(newfunc)
+
+    @classmethod
+    def inputTypeDict(cls, newfunc):
+        argData = inspect.getfullargspec(newfunc)
+        argList = argData.args
+        typeDict = argData.annotations
+        return {argVal: typeDict.get(argVal, str) for argVal in argList}
+
+
+class AutoServer(TemplateBase):
     def __init__(self):
         self.app = FastAPI()
-        jinjaPath = pathlib.Path(__file__).resolve().parent / 'resources/'
-        jinjaEnv = jinja2.Environment(loader=jinja2.FileSystemLoader(jinjaPath))
-        self.homepage = jinjaEnv.get_template("homepage.html.jinja2")
-        self.funcList = []
+        self.funcDataList = []
 
     def addfunc(self, newfunc):
-        self.funcList.append(newfunc)
+        self.funcDataList.append(funcData(newfunc))
+        return newfunc
 
     def run(self):
         @self.app.get("/",response_class=fastapi.responses.HTMLResponse)
         def read_root():
-            return self.homepage.render(fnList=self.funcList)
+            return self.homepage.render(fnList=self.funcDataList)
 
         uvicorn.run(self.app,
                     host="127.0.0.1",
@@ -29,8 +47,9 @@ if __name__ == "__main__":
     app = AutoServer()
 
     @app.addfunc
-    def testfunc1(exampleArg):
+    def testfunc1(arg1, arg2: int, arg3: float):
         pass
+
 
     app.run()
 
