a8f53435dd795a1ed7537a2fd5c65d4ddedcbe43
JanukanS <28988453+JanukanS@users.noreply.github.com>
2023-01-07 13:30:51 -0700

    Able to generate forms based on functions

--- autoserver/server.py
+++ autoserver/server.py
@@ -4,13 +4,16 @@ import uvicorn
 import jinja2
 import pathlib
 import inspect
+import collections
 
 class TemplateBase():
     jinjaPath = pathlib.Path(__file__).resolve().parent / 'resources/'
     jinjaEnv = jinja2.Environment(loader=jinja2.FileSystemLoader(jinjaPath))
     homepage = jinjaEnv.get_template("homepage.html.jinja2")
+    baseform = jinjaEnv.get_template("baseform.html.jinja2")
 
 class funcData(TemplateBase):
+    formDatum = collections.namedtuple("formDatum", "varName varType")
     def __init__(self, newfunc):
         self.fn = newfunc
         self.name = newfunc.__name__
@@ -22,6 +25,14 @@ class funcData(TemplateBase):
         argList = argData.args
         typeDict = argData.annotations
         return {argVal: typeDict.get(argVal, str) for argVal in argList}
+
+    @classmethod
+    def createForm(cls, newfunc):
+        typeDict = cls.inputTypeDict(newfunc)
+        formData = [cls.formDatum(argVal, typeDict[argVal].__name__) for argVal in typeDict]
+        return cls.baseform.render(formRows=formData)
+
+
 
 
 class AutoServer(TemplateBase):
@@ -50,6 +61,8 @@ if __name__ == "__main__":
     def testfunc1(arg1, arg2: int, arg3: float):
         pass
 
+    x=funcData.createForm(testfunc1)
+    print(x)
 
     app.run()
 
